a63b5bc529df4fb25f2934b4c9626439ff3b5a60
bnsuper <2464905469@qq.com>
2018-05-24 18:40:44 +0800

    add custom admin site

--- web/cms/admin.py
+++ web/cms/admin.py
@@ -2,8 +2,13 @@ from django.contrib import admin
 from mytest.models import test_Model
 
 # Register your models here.
+def make_published(modeladmin,request,queryset):
+	queryset.update(status='p')
+make_published.short_description = 'Mark selected stories as published'
 
 class AuthorAdmin(admin.ModelAdmin):
-	pass
+	list_display=['username','email','status']
+	ordering = ['username']
+	actions = [make_published]
 
 admin.site.register(test_Model,AuthorAdmin)
