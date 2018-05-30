fcd60b7bb0df3b06336e3c70abaca7224890df38
bnsuper <2464905469@qq.com>
2018-05-30 10:53:13 +0800

    modify cms/view

--- web/cms/views.py
+++ web/cms/views.py
@@ -5,11 +5,24 @@ from cms.forms import cms_loginForm
 from django.views.decorators.http import require_http_methods
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth import authenticate,login
+from django.contrib.sessions.models import Session
+from django.forms.models import model_to_dict
+
 # Create your views here.
 
 @login_required
 def cms_index(request):
-	print('index comming!')
+	# sessionid = request.COOKIES.get('sessionid')
+	# print('-'*10)
+	# sqlsession = Session.objects.filter(session_key=sessionid).first()
+	# print('sqlsession:',model_to_dict(sqlsession))
+	# print('-'*10)
+	# request.session['chenbin'] = 998
+	# print('request.session:',request.session.items())
+	# print('-'*10)
+	# print('request.session.session_key:',request.session.session_key)
+	# print('-'*10)
+	# print(dir(request.session))
 	return HttpResponse('这里是后台管理系统首页！')
 
 @require_http_methods(['GET','POST'])
@@ -24,15 +37,12 @@ def cms_login(request):
 			remember = form.cleaned_data.get('remember')
 			next_url  = request.GET.get('next')
 			user = authenticate(username=username,password=password)
-			print('------------')
 			if user is not None:
 				login(request,user)
 				if remember:
 					request.session.set_expiry(None)
 				else:
 					request.session.set_expiry(0)
-				print('++++++++++++++')
-				# return HttpResponse('登录成功')
 				if next_url:
 					return redirect(next_url)
 				else:
