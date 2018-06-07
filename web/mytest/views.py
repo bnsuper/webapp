982f0c47143ae61ff9c514b795d0d270f4370320
bn <2464905469@qq.com>
2018-06-07 22:32:53 +0800

    test cache and  caches

--- web/mytest/views.py
+++ web/mytest/views.py
@@ -3,8 +3,12 @@ from django.http import HttpResponse
 from django.shortcuts import reverse,redirect
 from mytest.models import test_Model
 from mytest.forms import testForm,testbForm
+from django.core.cache import caches,cache
 # Create your views here.
 
 def mytest_index(request):
+	cache.set('my_key', 'hello, world!', 30)
+	print('cache value:',cache.get('my_key'))
+	print('caches:',caches.all())
 	form = testbForm()
 	return render(request,'test.html',context={'form':form})
--- web/web/settings.py
+++ web/web/settings.py
@@ -91,6 +91,7 @@ DATABASES = {
 CACHES = {
     'default': {
         'BACKEND': 'django.core.cache.backends.memcached.MemcachedCache',
+        'LOCATION':'127.0.0.1:11211',
     }
 }
 
